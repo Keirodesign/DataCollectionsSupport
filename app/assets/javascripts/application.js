9f964ac6f9fce09defb83fdd369b5b683a03b1c7
Arif Liya <arifliya@gmail.com>
2019-01-25 18:31:35 +0000

    updates with filters

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,4 +7,44 @@ if (window.console && window.console.info) {
 
 $(document).ready(function () {
   window.GOVUKFrontend.initAll()
-})
+});
+
+// $('#ILR').click(function() {
+//     if ($(this).is(':checked')) {
+//     $('#submissions tr').hide();
+//     $('.ilr-row').show();
+//   } else {
+//     $('#submissions tr').show();
+//   }
+// });
+//
+// $('#ESF').click(function() {
+//     if ($(this).is(':checked')) {
+//     $('#submissions tr').hide();
+//     $('.esf-row').show();
+//   } else {
+//     $('#submissions tr').show();
+//   }
+// });
+
+$("#typeFilter :checkbox").click(function() {
+    $("#submissions tr").hide();
+    $("#typeFilter :checkbox:checked").each(function() {
+        $("." + $(this).val()).show();
+    });
+
+    if (!$("#typeFilter :checkbox").is(':checked')) {
+      $("#submissions tr").show();
+    }
+});
+
+$("#yearFilter :checkbox").click(function() {
+    $(".year-groups").hide();
+    $("#yearFilter :checkbox:checked").each(function() {
+        $("." + $(this).val()).show();
+    });
+
+    if (!$("#yearFilter :checkbox").is(':checked')) {
+      $(".year-groups").show();
+    }
+});
